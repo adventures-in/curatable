c68008194c1178016abecaec3b785379cab1d023
Nick Meinhold <nick.meinhold@gmail.com>
2021-08-27 19:02:14 +1000

    Add ability to save to firestore (#2)
    * Add googleapis dependency
    
    * Use googleapis to save to firestore
    
    - used FirestoreApi to create a new document each time
    the cloud run instance is accessed

--- backend/lib/functions.dart
+++ backend/lib/functions.dart
@@ -1,5 +1,30 @@
+import 'dart:convert';
+
 import 'package:functions_framework/functions_framework.dart';
+import 'package:googleapis/firestore/v1.dart';
+import 'package:googleapis_auth/auth_io.dart';
 import 'package:shelf/shelf.dart';
 
+const _encoder = JsonEncoder.withIndent(' ');
+
 @CloudFunction()
-Response function(Request request) => Response.ok('Hello, World!');
+Future<Response> function(Request request) async {
+  // Create services and a client that will authenticate as the given user.
+  final serviceClient =
+      await clientViaApplicationDefaultCredentials(scopes: []);
+  final api = FirestoreApi(serviceClient);
+
+  final requestDocument = Document()
+    ..fields = {'key': Value()..integerValue = '5'};
+
+  final responseDocument =
+      await api.projects.databases.documents.createDocument(
+    requestDocument,
+    'projects/news-curation-project/databases/(default)/documents',
+    'testing',
+  );
+
+  print(_encoder.convert(responseDocument.toJson()));
+
+  return Response.ok('Saved x new posts');
+}
