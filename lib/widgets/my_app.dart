06082319e3845ad791eb91aff6104a44fe7badd3
Nick Meinhold <nick.meinhold@gmail.com>
2021-08-30 20:28:59 +1000

    Move MyApp to separate file
    - vs code sneakily generated mac and ios files

---
+++ lib/widgets/my_app.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _itemCount = 20;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () => Future.delayed(
+            const Duration(seconds: 3), () => setState(() => _itemCount += 20)),
+        child: ListView.builder(
+            itemCount: _itemCount,
+            itemBuilder: (_, index) => SizedBox(
+                  height: 100,
+                  child: Card(
+                    child: Center(child: Text(index.toString())),
+                  ),
+                )),
+      ),
+    );
+  }
+}
