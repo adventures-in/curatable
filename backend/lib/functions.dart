fef984003c78737be00b38fa25aee9621481813e
Nick Meinhold <nick.meinhold@gmail.com>
2021-09-09 15:07:19 +1000

    Clear out reddit-post docs and save new set
    - cleaned up and moved into services

--- backend/lib/functions.dart
+++ backend/lib/functions.dart
@@ -1,38 +1,28 @@
-import 'dart:convert';
-
 import 'package:backend/services/firestore_service.dart';
-import 'package:backend/utils/type_utils.dart';
+import 'package:backend/services/http_service.dart';
 import 'package:functions_framework/functions_framework.dart';
 import 'package:googleapis_auth/auth_io.dart';
-import 'package:http/http.dart' as http;
 import 'package:shelf/shelf.dart';
 
 @CloudFunction()
 Future<Response> function(Request request) async {
+  // try {
+
+  // } catch (error) {
+  //   print(error);
+  //   return Response.internalServerError(body: error);
+  // }
+
   // Create a client that will authenticate as the default service account.
   final googleapisClient =
       await clientViaApplicationDefaultCredentials(scopes: []);
 
-  final service = FirestoreService(googleapisClient);
+  final firestoreService = FirestoreService(googleapisClient);
+  final httpService = HttpService();
 
-  var client = http.Client();
-  final queryParameters = {'sort': 'new', 'limit': '100', 'raw_json': '1'};
-  final uri =
-      Uri.https('www.reddit.com', '/r/FlutterDev/.json', queryParameters);
+  final latestRedditPosts = await httpService.retrieveRedditPosts();
 
-  try {
-    final response =
-        await http.get(uri, headers: {'content-type': 'application/json'});
-
-    // Decode the response to a JsonMap and save each reddit post to a firestore document
-    final json = JsonDecoder().convert(response.body);
-    final jsonList = json['data']['children'] as List;
-    for (var message in jsonList) {
-      service.saveDocument(message['data'] as JsonMap);
-    }
-  } finally {
-    client.close();
-  }
+  await firestoreService.mergeRedditPosts(latestRedditPosts);
 
   return Response.ok('Saved x new posts');
 }
--- backend/lib/services/firestore_service.dart
+++ backend/lib/services/firestore_service.dart
@@ -2,20 +2,36 @@ import 'package:backend/utils/type_utils.dart';
 import 'package:googleapis/firestore/v1.dart';
 import 'package:googleapis_auth/auth_io.dart';
 
+/// console: https://console.firebase.google.com/project/news-curation-project/firestore/data
+// we will hit this endpoint every (say) hour, with a cron job: https://cloud.google.com/scheduler
 class FirestoreService {
   FirestoreService(AutoRefreshingAuthClient client, {FirestoreApi? api}) {
-    _api = api ?? FirestoreApi(client);
+    _api = api ?? FirestoreApi(client, rootUrl: 'http://localhost:8081/');
+    _docs = _api.projects.databases.documents;
   }
 
   late final FirestoreApi _api;
+  late final ProjectsDatabasesDocumentsResource _docs;
+  static const _redditPostsCollectionName = 'reddit-posts';
+  static const _databaseName =
+      'projects/news-curation-project/databases/(default)/documents';
 
-  saveDocument(JsonMap json) async {
-    final requestDocument = json.toDocument();
+  Future<Document> _saveRedditPost(JsonMap json) async => _docs.createDocument(
+        json.toDocument(),
+        _databaseName,
+        _redditPostsCollectionName,
+      );
 
-    await _api.projects.databases.documents.createDocument(
-      requestDocument,
-      'projects/news-curation-project/databases/(default)/documents',
-      'testing',
-    );
+  Future<void> mergeRedditPosts(List<dynamic> jsonList) async {
+    // clear out all docs
+    var response = await _docs.list(_databaseName, _redditPostsCollectionName);
+    for (var doc in response.documents ?? []) {
+      await _docs.delete(doc.name);
+    }
+
+    // save new docs
+    for (dynamic redditPost in jsonList) {
+      await _saveRedditPost(redditPost['data'] as JsonMap);
+    }
   }
 }
